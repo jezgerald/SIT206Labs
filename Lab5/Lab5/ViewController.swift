bcbe43a640eb763fa8c2d1e075adedd86f563efa
Jessica Fitzgerald <jessicafitzgerald@jessicas-imac.lan>
2018-04-22 17:43:02 +1000

    Version 1

---
+++ Lab5/Lab5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab5
+//
+//  Created by Jessica Fitzgerald on 22/4/18.
+//  Copyright © 2018 Jessica Fitzgerald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
